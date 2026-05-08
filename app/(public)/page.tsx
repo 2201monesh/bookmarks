e7095273152853d073b40239bc9c4d5afb714a7d
Monesh Goyal <goyalmonesh2201@gmail.com>
2026-05-09 02:23:00 +0530

    feat: added shadcn btns on / route screen

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,7 +1,23 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1>Hello 1234567</h1>
+    <div className="flex py-8 justify-center h-screen">
+        <div className="min-w-6xl h-full px-6 flex flex-col">
+            <div className="flex items-center justify-between">
+                <h1>Bookmarks</h1>
+                <div className="flex items-center gap-2">
+                    <Button variant="outline" size="sm" className="shadow-none">Login</Button>
+                    <Button size="sm" className="shadow-none">Get Started</Button>
+                </div>
+            </div>
+            <div className="py-14">
+                <p className="text-2xl">Built for deep thinking</p>
+                <p className="text-2xl">Compile is the best place</p>
+                <p className="text-2xl">for your ideas to live</p>
+                <Button size="sm" className="mt-4 shadow-none">Start Bookmarking your links</Button>
+            </div>
+        </div>
     </div>
   );
 }
